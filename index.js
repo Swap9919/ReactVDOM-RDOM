930cac6a76b73a850b2a88af3d0c800fa8c8da29
Sai Swaroop <saiswaroop874@gmail.com>
2023-02-03 13:26:30 +0300

    vDom

---
+++ index.js
@@ -0,0 +1,24 @@
+
+function setTimer() {
+      const element1 = `
+  <div>
+  <h1>JavaScript</h1>
+  <input type="text" />   
+  <p>${new Date().toLocaleTimeString()}</p> 
+  </div>
+`;
+document.getElementById("container1").innerHTML = element1;
+
+      const element2 = React.createElement(
+            'div',
+            null,
+            React.createElement('h1', null, 'React'),
+            React.createElement('div', null,
+            React.createElement('input', { type: 'text' })
+            ),
+            React.createElement('p', null, new Date().toLocaleTimeString())
+            );
+      ReactDom.render(element2, document.getElementById("container2"))
+}
+setInterval(setTimer, 1000);
+
